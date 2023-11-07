d26d328ebfeafff46e13d259e29999d7171d7e93
tshepoandi <lovekilledtshepo@gmail.com>
2023-11-07 14:53:30 +0200

    popular styling

--- src/Components/Popular.jsx
+++ src/Components/Popular.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
 
 function Popular() {
     const [popular,setPopular] = useState([])
@@ -6,25 +7,37 @@ function Popular() {
         getPopular()
     },[])
     const getPopular = async()=>{
-      try {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=15`)
-        const data = await api.json();
-        console.log(data)
-        setPopular(data.recipes)
-      } catch (error) {
-        setPopular([{title:"problems reaching uhmmm....yeah"}])
-      }
-      
+      const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=15`)
+      const data = await api.json();
+      console.log(data)
+      setPopular(data.recipes)      
     } 
   return (
     <div>
+      <h3>Popular Picks</h3>
+      <Wrapper>
       {popular.map((recipe,index)=>{
         return (
+          <Card>
             <p key={index}>{recipe.title}</p>
+            <img src={recipe.image} alt={recipe.title} />
+          </Card>            
         )
       })}
+      </Wrapper>
     </div>
   )
 }
+
+export const Wrapper = styled.div`
+  margin: 4rem 0rem;  
+`
+
+export const Card = styled.div`
+  min-height: 25rem;
+  img {
+    border-radius:2rem;
+  }
+`
 
 export default Popular
