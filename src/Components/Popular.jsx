bbe7f4ea8044f6652f28f31b38f629ea035a9dee
tshepoandi <lovekilledtshepo@gmail.com>
2023-11-07 12:50:52 +0200

    popular try catch

--- src/Components/Popular.jsx
+++ src/Components/Popular.jsx
@@ -1,17 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Popular() {
+    const [popular,setPopular] = useState([])
     useEffect(()=>{
         getPopular()
     },[])
     const getPopular = async()=>{
+      try {
         const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=15`)
         const data = await api.json();
         console.log(data)
+        setPopular(data.recipes)
+      } catch (error) {
+        setPopular([{title:"problems reaching uhmmm....yeah"}])
+      }
+      
     } 
   return (
     <div>
-      popular
+      {popular.map((recipe,index)=>{
+        return (
+            <p key={index}>{recipe.title}</p>
+        )
+      })}
     </div>
   )
 }
