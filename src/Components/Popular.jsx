c9f39e8e58dd19d795465305a1262c3cbcda5ec7
tshepoandi <lovekilledtshepo@gmail.com>
2023-11-07 18:05:17 +0200

    popular styling

--- src/Components/Popular.jsx
+++ src/Components/Popular.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import { Splide,SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/splide/css';
 function Popular() {
     const [popular,setPopular] = useState([])
@@ -16,14 +17,28 @@ function Popular() {
     <div>
       <h3>Popular Picks</h3>
       <Wrapper>
-      {popular.map((recipe,index)=>{
-        return (
-          <Card>
-            <p key={index}>{recipe.title}</p>
-            <img src={recipe.image} alt={recipe.title} />
-          </Card>            
-        )
-      })}
+        <Splide
+
+           options={{
+            perPage:4,
+            arrows:false,
+            pagination:false,
+            drag:"free",
+            gap:"5rem"
+          }}
+        >
+          {popular.map((recipe,index)=>{
+            return (
+              <SplideSlide key={index}>
+                <Card>
+                  <Paragraph key={index}>{recipe.title}</Paragraph>
+                  <Image src={recipe.image} alt={recipe.title} />
+                  <Gradient/>
+                </Card>    
+              </SplideSlide>           
+          )
+        })}
+        </Splide>
       </Wrapper>
     </div>
   )
@@ -33,11 +48,42 @@ export const Wrapper = styled.div`
   margin: 4rem 0rem;  
 `
 
-export const Card = styled.div`
-  min-height: 25rem;
-  img {
-    border-radius:2rem;
-  }
+const Gradient = styled.div`
+  z-index:3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.5))
 `
 
+export const Card = styled.div`
+  min-height: 25rem;
+  overflow: hidden;
+  position:relative;
+`
+const Image = styled.img`
+  position:absolute;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover; 
+  border-radius:2rem;
+`
+
+const Paragraph = styled.p`
+position:absolute;
+z-index:10;
+left:50%;
+bottom:0%;
+transform:translate(-50%, 0%);
+color:white;
+width:100%;
+text-align:center;
+font-weight:600;
+font-size:1rem;
+height:40%;
+display:flex;
+justify-content:center;
+align-items:center;
+`
 export default Popular
