88e7bfcbbd38384573db4b31fc0afd6e2a7105dc
tshepoandi <lovekilledtshepo@gmail.com>
2023-11-08 09:55:32 +0200

    search bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Category from "./Components/Category";
 // import Pages from "./Pages/Pages";
 import Home from "./Pages/Home";
 import Cuisine from "./Pages/Cuisine";
+import Search from "./Components/Search";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Search/>
         <Category/>
         <Routes>
           <Route path="/" element={<Home/>}/>
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import {FaSearch} from 'react-icons/fa'
 
 function Search() {
   return (
-    <div>
+    <FormStyle>
+      <NeededDiv>
+      <StyledSVG></StyledSVG>
+      <Input type='text'/>
+      </NeededDiv>
       
-    </div>
+    </FormStyle>
   )
 }
+
+const FormStyle = styled.form`
+  width:100%;
+`
+
+
+const NeededDiv = styled.div`
+  width: 100%;
+  position:relative
+`
+
+const Input = styled.input`
+margin-left:3rem;
+padding:1rem;
+border:none;
+background: linear-gradient(35deg,#494949,#313131);
+border-radius:2rem;
+outline:none;
+color:white;
+width:100%;
+`
+
+const StyledSVG = styled(FaSearch)`
+position: absolute;
+top :50%;
+left:0;
+transform: translate(100%,-50%);
+color:black;
+
+
+`
 
 export default Search
