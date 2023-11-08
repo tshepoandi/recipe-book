2e2c8d65ed9c4cf127cabb8434adcf12ff6bc06c
tshepoandi <lovekilledtshepo@gmail.com>
2023-11-08 10:59:55 +0200

    search

--- src/Pages/handleSearch.jsx
+++ src/Pages/handleSearch.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import styled from 'styled-components'
 
 function HandleSearch() {
-  return (
-    <div>
-      <h1>okay</h1>
-    </div>
+
+    const [HandleSearch,setHandleSearch] = useState([]);
+    let params = useParams()
+
+    const getHandleSearch = async (name) =>{
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&number=50&query=${name}`)
+        const recipes = await data.json()
+        setHandleSearch(recipes.results)
+        console.log(recipes.results)
+
+    };
+    useEffect(()=>{
+        getHandleSearch(params.type)
+    },[params.type])
+    // console.log(params)
+    return (
+    <FoodGrid>
+        {HandleSearch.map((item)=>{
+            return (
+                
+                <Card key={item.id}>
+                    <Image src={item.image}/>
+                    <Hfour>{item.title}</Hfour>
+                </Card>
+            )
+        })}
+    </FoodGrid>
   )
 }
 
+const Card = styled.div`
+   
+    
+`
+const FoodGrid = styled.div`
+
+display: grid;
+grid-template-columns: repeat(3, 1fr);
+    grid-gap: 3rem;
+    @media (max-width: 768px) {
+        grid-template-columns: repeat(1, 1fr);  /* 2 images in each row on smaller screens */
+    }
+    
+`
+
+const Image = styled.img`
+    width:100%;
+    border-radius:2rem;
+`
+const Anchor = styled.a`
+    text-decoration:none;
+    `
+const Hfour = styled.h4`
+    font-size:2rem;
+    font-weight:700;
+    text-align:center;
+    padding: 1rem;`
 export default HandleSearch
+
